b1eaea8f13610725bfadb30d7d5742f174379de2
Andy <andy.mallcco@tecsup.edu.pe>
2024-10-15 23:29:40 -0500

    añadido el campo de imagen

--- inventario/models.py
+++ inventario/models.py
@@ -8,6 +8,7 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)  # Precio del producto
     descripcion = models.TextField()  # Descripción del producto
     stock = models.IntegerField()  # Cantidad de stock disponible
+    foto = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
